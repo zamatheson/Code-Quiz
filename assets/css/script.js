cdf22e4d2946d52c079a488ad90c117329901ae2
Za Matheson <zamatheson@Zas-MacBook-Air.local>
2023-09-30 10:36:53 -0500

    fixing html and adding javascript

---
+++ assets/css/script.js
@@ -0,0 +1,11 @@
+const questions [
+    {
+        title: "Commonly used data types DO NOT include...",
+        choices: [
+            { text: "strings", answer: false},
+            { text: "booleans", answer: false},
+            { text: "alerts", answer: false},
+            { text: "numbers", answer: false},
+        ]  
+    }
+]
