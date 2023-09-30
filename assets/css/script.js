e099075ee26effb843dadc1f5e594a9e9ba8bbb5
Za Matheson <zamatheson@Zas-MacBook-Air.local>
2023-09-30 10:37:48 -0500

    more javascript, forgot to save before last push

--- assets/css/script.js
+++ assets/css/script.js
@@ -3,9 +3,45 @@ const questions [
         title: "Commonly used data types DO NOT include...",
         choices: [
             { text: "strings", answer: false},
-            { text: "booleans", answer: false},
+            { text: "booleans", answer: true},
             { text: "alerts", answer: false},
             { text: "numbers", answer: false},
         ]  
+    },
+    {
+        title: "The condition in an if/else statement is enclosed with...",
+        choices: [
+            { text: "quotes", answer: false},
+            { text: "curly brackets", answer: true},
+            { text: "paraenthesis", answer: false},
+            { text: "square brackets", answer: false},
+        ]   
+    },
+    {
+        title: "Arrays in JavaScript can be used to store...",
+        choices: [
+            { text: "numbers and strings", answer: false},
+            { text: "other arrays", answer: false},
+            { text: "booleans", answer: true},
+            { text: "all of the above", answer: true},
+        ]  
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables...",
+        choices: [
+            { text: "commas", answer: false},
+            { text: "curly brackets", answer: false},
+            { text: "quotes", answer: true},
+            { text: "parenthesis", answer: false},
+        ]  
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is...",
+        choices: [
+            { text: "JavaScript", answer: true},
+            { text: "terminal/bash", answer: false},
+            { text: "for loops", answer: false},
+            { text: "console.log", answer: false},
+        ]  
     }
-]
+];
